229087a7776e05ec75b3e479da4bc0621edc06d2
Luigi Armani <133150085+personaluigi@users.noreply.github.com>
2023-12-29 10:15:17 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
-const text = document.getElementById('advice-msg');
-const textId = document.getElementById('advice-id');
-const btn = document.getElementById('btn-dice');
+const text = document.getElementById('advice-msg')
+const textId = document.getElementById('advice-id')
+const btn = document.getElementById('btn-dice')
 
 fetch('https://api.adviceslip.com/advice')
     .then((resp) => resp.json())
     .then((data) => {
-        const {id, advice} = data.slip;
-        textId.innerText = `ADVICE #${id}`;
-        text.innerText = `"${advice}"`;
+        const { id, advice } = data.slip
+        textId.innerText = `ADVICE #${id}`
+        text.innerText = `"${advice}"`
     })
 
-function reload() {
-    location.reload();
+const reload = () => {
+    fetch('https://api.adviceslip.com/advice')
+        .then((resp) => resp.json())
+        .then((data) => {
+            const { id, advice } = data.slip
+            textId.innerText = `ADVICE #${id}`
+            text.innerText = `"${advice}"`
+        })
+
 }
 
-btn.addEventListener('click', reload);
+const diceAnimation = () => {
+    setTimeout(() => {
+        document.querySelector('.btn-dice').classList.toggle('btn-dice-active')
+    }, 600)
+}
+
+const animation = () => {
+    diceAnimation()
+    reload()
+}
+
+btn.addEventListener('click', animation)
